8e64a59a3aeacbbe25f972b8a7148e4b43111eba
ssaim <ssaimanjunath@gmail.com>
2019-10-08 21:06:03 +0530

    Final Commit

--- migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Forensic = artifacts.require("./ForensicContract.sol");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(Forensic);
-};
